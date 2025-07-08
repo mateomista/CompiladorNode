c61190f6491e4c83fb33d5234e8d2f6e12a4e808
Mateo Mista <mateomista5@gmail.com>
2025-07-08 15:14:29 -0300

    termino automatas

---
+++ src/lexer/automatas/esOperadorRelacional.js
@@ -0,0 +1,70 @@
+import { token } from '../token.js';
+
+export function esOperadorRelacional(fuente, pos) {
+    let i = pos;
+    const longitud = fuente.length;
+    let lexema = '';
+
+    if (i >= longitud) {
+        return null; // No hay caracteres para procesar
+    }
+
+    if (fuente[i] === '<') {
+        lexema += fuente[i];
+        i++;
+
+        if (i < longitud && fuente[i] === '=') {
+            lexema += fuente[i];
+            i++;
+            return {
+                token: new token('opRel', lexema),
+                nuevaPos: i
+            }; // es un operador menor o igual
+        }
+
+        if (i < longitud && fuente[i] === '>') {
+            lexema += fuente[i];
+            i++;
+            return {
+                token: new Token('opRel', lexema),
+                nuevaPos: i
+            }; // es un operador diferente
+        }
+
+        return {
+            token: new Token('opRel', lexema),
+            nuevaPos: i
+        }; // es un operador menor
+    }
+
+    if (fuente[i] === '>') {
+        lexema += fuente[i];
+        i++;
+
+        if (i < longitud && fuente[i] === '=') {
+            lexema += fuente[i];
+            i++;
+            return {
+                token: new Token('opRel', lexema),
+                nuevaPos: i
+            }; // es un operador mayor o igual
+        }
+
+        return {
+            token: new Token('opRel', lexema),
+            nuevaPos: i
+        }; // es un operador mayor
+    }
+
+    if (fuente[i] === '=') {
+        lexema += fuente[i];
+        i++;
+        return {
+            token: new Token('opRel', lexema),
+            nuevaPos: i
+        }; // es un operador de igualdad
+    }
+
+    return null; // No es un operador relacional
+
+}
