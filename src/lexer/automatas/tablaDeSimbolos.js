45b2939b79e8171a575b3e97ea3be8a0acd2996f
Mateo Mista <mateomista5@gmail.com>
2025-07-08 23:29:54 -0300

    agrego tabla de simbolos

---
+++ src/lexer/automatas/tablaDeSimbolos.js
@@ -0,0 +1,41 @@
+import { Token } from '../token.js';
+
+const palabrasReservadas = [
+  "Program",
+  "end",
+  "Read",
+  "Write",
+  "if",
+  "then",
+  "else",
+  "while",
+  "do",
+  "for",
+];
+
+export class TablaDeSimbolos {
+  constructor(array = palabrasReservadas) {
+    this.simbolos = [];
+    array.forEach(element => {
+      this.simbolos.push(new Token(element, element));
+    });
+  }
+
+  agregar(token) {
+    if (!this.simbolos.some(t => t.lexema === token.lexema)) {
+      this.simbolos.push(token);
+    }
+  }
+
+  obtener(lexema) {
+    return this.simbolos.find(t => t.lexema === lexema) || null;
+  }
+
+  contiene(lexema) {
+    return this.simbolos.some(t => t.lexema === lexema);
+  }
+
+  limpiar() {
+    this.simbolos = [];
+  }
+}
